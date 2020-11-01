39dff119dfb5fe4e3ee715f71c771229e4a9db14
Ivan <matteraivam@gmail.com>
2020-11-01 10:38:48 +0100

    esercizio fatto con vue js

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
-$("a").click (function() {
-  $(".header-left , .header-right").hide();
-  $(".hamburger-menu").show();
-});
+// $(".bar").click (function() {
+//   $(".header-left , .header-right").hide();
+//   $(".hamburger-menu").show();
+// });
+//
+// $(".close").click (function() {
+//   $(".header-left , .header-right").show();
+//   $(".hamburger-menu").hide();
+// });
 
-$(".close").click (function() {
-  $(".header-left , .header-right").show();
-  $(".hamburger-menu").hide();
+
+const app = new Vue ({
+  el : '#bar',
+  data:{
+      bar : false
+  },
+  methods: {
+    showMenu(){
+      this.bar = true;
+    },
+    closeMenu(){
+      this.bar = false;
+    }
+  }
 });
